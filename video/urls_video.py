481c1e42a64fc68b8e63859d25e5c8c88d60b366
Sanello <sanello@gmail.com>
2019-08-01 12:39:13 +0300

    nothing has changed

--- video/urls_video.py
+++ video/urls_video.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path('video/', views.index_vid, name='index_vid')
+    re_path('video/', views.index_vid, name='/video/index_vid')
 ]
--- video/views.py
+++ video/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def index_vid(request):
-    return render(request, 'video/index_vid.html')
+    return render(request, '/video/index_vid.html')
 
 # Create your views here.
